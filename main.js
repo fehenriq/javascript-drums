9b3f237c0b62f1ce74b3bf51f8e689d4b38ab0fb
ti7 <ti7@gimipogliano.local>
2022-09-06 11:55:23 -0300

    fixed problems with alert

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-function tocaSom(seletorAudio) {
-    const elemento = document.querySelector(seletorAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
     if (elemento && elemento.localName === 'audio') {
         elemento.play();
-    } else {
-        alert('Elemento não encontrado ou seletor inválido!');
     }
+    else {
+        alert('Elemento não encontrado ou seletor inválido');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for (let cont = 0; cont < listaDeTeclas.length; cont++) {
-    const tecla = listaDeTeclas[cont];
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`; //template string
 
@@ -20,12 +24,15 @@ for (let cont = 0; cont < listaDeTeclas.length; cont++) {
     }
 
     tecla.onkeydown = function (evento) {
+
         if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
+
     }
 
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
+
 }
